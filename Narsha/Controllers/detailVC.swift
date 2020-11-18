f320f05eaf4323288173daa1a0eda1bd5b86ee26
apple19760401 <bs2740@naver.com>
2020-11-19 08:57:47 +0900

    카테고리, 주소 를 디테일 화면에 띄우고, 네이버 지도 앱에서 열기 추가됨

--- Narsha/Controllers/detailVC.swift
+++ Narsha/Controllers/detailVC.swift
@@ -11,7 +11,11 @@ import NMapsMap
 class detailVC: UIViewController {
     
     @IBOutlet weak var namelbl: UILabel!
+    @IBOutlet weak var catagorylbl: UILabel!
     @IBOutlet weak var mapView: NMFMapView!
+    @IBOutlet weak var adlbl: UILabel!
+    @IBOutlet weak var navermapbtn: UIButton!
+    
     
     let delegate = UIApplication.shared.delegate as? AppDelegate
     
@@ -19,21 +23,41 @@ class detailVC: UIViewController {
         super.viewDidLoad()
         self.navigationItem.title = delegate?.data?["name"] as? String
         namelbl.text = navigationItem.title
+        var catagory = delegate?.data?["category"] as? String
+        let address = delegate?.data?["address"] as? String
+        
+        switch catagory {
+        case "rest":
+            catagory = "음식점"
+        case "cafe":
+            catagory = "카페"
+        case "mart":
+            catagory = "마트"
+        default:
+            break
+        }
+        
+        catagorylbl.text = "카테고리 : \(catagory!)"
+        adlbl.text = "주소 : \(address!)"
         let location_x = delegate?.data?["coord_x"] as? Double
         let location_y = delegate?.data?["coord_y"] as? Double
         
-        print(location_x as Any)
-        print(location_y as Any)
         let marker = NMFMarker()
         marker.position = NMGWebMercatorCoord(x: location_x!, y: location_y!).toLatLng()
         marker.mapView = mapView
         marker.captionText = (delegate?.data!["name"] as? String)!
-        mapView.buildingHeight = 1
         mapView.setLayerGroup(NMF_LAYER_GROUP_TRANSIT, isEnabled: true)
         let cameraUpdate = NMFCameraUpdate(scrollTo: NMGWebMercatorCoord(x: location_x!, y: location_y!).toLatLng())
         mapView.moveCamera(cameraUpdate)
+        mapView.logoInteractionEnabled = true
+        mapView.positionMode = .direction
         
         // Do any additional setup after loading the view.
+    }
+    @IBAction func navermapbtnclicked(_ sender: Any) {
+        if let url = URL(string: "nmap://place?lat=\((delegate?.data?["latitude"] as? Double)!)&lng=\((delegate?.data?["longitude"] as? Double)!)&name=\(namelbl.text!)&appname=kr.hs.dgsw.Narsha.guji") {
+            UIApplication.shared.open(url, options: [:])
+        }
     }
     
 //    override func viewWillAppear(_ animated: Bool) {
